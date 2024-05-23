0b83e76fdc324b7c5ad679ab3dedcdb0b82b7376
weidl <felix_weidner@web.de>
2024-05-23 22:47:32 +0200

    added usage of db to prevent multiposting of the same items, added filtersystem for multiple filters on the same url

--- request_test.py
+++ request_test.py
@@ -1,5 +1,6 @@
 import time
 import os
+import sqlite3
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
@@ -9,24 +10,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from discord_webhook import DiscordWebhook, DiscordEmbed
+import json
 
 class Config:
     def __init__(self, config_file):
         self.config_file = config_file
         self.webhook_url = None
+        self.filter_file = None
         self.load_config()
 
     def load_config(self):
         with open(self.config_file, 'r') as file:
-            for line in file:
-                key, value = line.strip().split(':', 1)
-                setattr(self, key, value.strip())
+            config = json.load(file)
+            self.webhook_url = config.get("webhook_url")
+            self.filter_file = config.get("filter_file")
+
+class Filter:
+    def __init__(self, filter_file):
+        self.filter_file = filter_file
+        self.url = None
+        self.attributes = []
+        self.conditions = []
+        self.load_filter()
+
+    def load_filter(self):
+        with open(self.filter_file, 'r') as file:
+            filter_config = json.load(file)
+            self.url = filter_config.get("url")
+            self.attributes = filter_config.get("attributes", [])
+            self.conditions = filter_config.get("conditions", [])
 
 def fetch_page(url):
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    driver.get(url)
+    driver.get(str(url))  # Ensure URL is a string
     
     try:
         WebDriverWait(driver, 10).until(
@@ -40,27 +58,29 @@ def fetch_page(url):
     with open("recent_fetch.html", "w", encoding="utf-8") as file:
         file.write(page_content)
     
-    return page_content
+    return driver, page_content
 
-def parse_page(content):
+def parse_page(driver, content, filter_config):
     soup = BeautifulSoup(content, 'html.parser')
     items = soup.find_all('div', class_='bg-[#111212] border-2 border-dim-yellow rounded-xl overflow-border-opacity-50 relative flex w-fit flex-1 flex-col gap-1 overflow-x-clip p-2 shadow-[0px_-10px_15px_0px#000a] transition-all duration-75 ui-state-revealed:animate-reveal-appear WTS')
     print(f"Anzahl der gefundenen Elemente: {len(items)}")
     
     results = []
 
-    for item in items:
+    for index, item in enumerate(items):
         title_element = item.find('div', class_='flex max-w-[250px] flex-wrap items-center gap-0.5 font-diablo text-xs font-bold uppercase sm:text-sm')
         title = title_element.text.strip() if title_element else "N/A"
         print(f'Title: {title}')
         
-        movement_speed_element = item.find('span', string=lambda text: text and "Movement Speed" in text)
-        movement_speed = movement_speed_element.text if movement_speed_element else "N/A"
-        print(f'Movement Speed: {movement_speed}')
+        user_element = item.find('button', class_='font-medium uppercase text-xs')
+        user = user_element.text.strip() if user_element else "N/A"
+        print(f'User: {user}')
         
-        damage_reduction_element = item.find('span', string=lambda text: text and "Damage Reduction from Close Enemies" in text)
-        damage_reduction = damage_reduction_element.text if damage_reduction_element else "N/A"
-        print(f'Damage Reduction from Close Enemies: {damage_reduction}')
+        attributes = {}
+        for attribute in filter_config.attributes:
+            attribute_element = item.find('span', string=lambda text: text and attribute in text)
+            attributes[attribute] = attribute_element.text if attribute_element else "N/A"
+            print(f'{attribute}: {attributes[attribute]}')
         
         footer_element = item.find_next('div', class_='bg-white/5 backdrop-blur centered -mb-2 -ml-2 flex w-[calc(100%_+_16px)] flex-col gap-0 rounded-b-lg border-t-2 border-dim-yellow py-2 relative')
         if footer_element:
@@ -69,7 +89,7 @@ def parse_page(content):
                 price_element = footer_element.find('h4', class_='font-pt-serif text-yellow-200')
                 price = price_element.text.strip() if price_element else "N/A"
             else:
-                make_offer_element = footer_element.find('h4', string="Make an offer!")
+                make_offer_element = footer_element.find('h6', string="Taking Offers")
                 price = make_offer_element.text.strip() if make_offer_element else "N/A"
         else:
             price = "N/A"
@@ -77,39 +97,90 @@ def parse_page(content):
         print(f'Price: {price}')
         
         print('-' * 40)
-        
-        if price != "N/A":
+
+        if is_item_relevant(attributes, filter_config.conditions):
             results.append({
                 'title': title,
-                'movement_speed': movement_speed,
-                'damage_reduction': damage_reduction,
+                'user': user,
+                'attributes': attributes,
                 'price': price
             })
 
     return results
 
+def is_item_relevant(attributes, conditions):
+    for condition_group in conditions:
+        match = True
+        for condition in condition_group.split(";"):
+            field, operator, value = condition.split(",")
+            value = float(value)
+            
+            if field in attributes and attributes[field] != "N/A":
+                attribute_value = float(attributes[field].split()[0].replace('%', '').replace(',', ''))
+                
+                if operator == ">" and not (attribute_value > value):
+                    match = False
+                elif operator == "<" and not (attribute_value < value):
+                    match = False
+                elif operator == ">=" and not (attribute_value >= value):
+                    match = False
+                elif operator == "<=" and not (attribute_value <= value):
+                    match = False
+                elif operator == "==" and not (attribute_value == value):
+                    match = False
+                elif operator == "!=" and not (attribute_value != value):
+                    match = False
+            else:
+                match = False
+        
+        if match:
+            return True
+    return False
+
 def send_discord_notification(item, webhook_url):
     webhook = DiscordWebhook(url=webhook_url)
     embed = DiscordEmbed(
         title=f"**{item['title']}**",
-        description=f"**Movement Speed:** {item['movement_speed']}\n**Damage Reduction:** {item['damage_reduction']}\n**Price:** {item['price']}",
+        description=f"**User:** {item['user']}\n\n" + "\n\n".join([f"**{key}:** {value}" for key, value in item['attributes'].items()]) + f"\n\n**Price:** {item['price']}",
         color=242424
     )
     webhook.add_embed(embed)
     response = webhook.execute()
 
 if __name__ == "__main__":
-    config = Config('config')
-    url = "https://diablo.trade/listings/items?cursor=21&group1=&rarity=unique&uniqueItem=yens-blessing"
+    config = Config('config.json')
+    filter_config = Filter(config.filter_file)
+    db_conn = sqlite3.connect('items.db')
+    db_cursor = db_conn.cursor()
+    db_cursor.execute('''
+        CREATE TABLE IF NOT EXISTS items (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            user TEXT,
+            attributes TEXT,
+            price TEXT,
+            UNIQUE(title, user, attributes, price)
+        )
+    ''')
+
+    url = filter_config.url
     
     while True:
-        page_content = fetch_page(url)
+        driver, page_content = fetch_page(url)
         if page_content:
-            interesting_items = parse_page(page_content)
+            interesting_items = parse_page(driver, page_content, filter_config)
             if interesting_items:
                 for item in interesting_items:
-                    send_discord_notification(item, config.webhook_url)
+                    try:
+                        db_cursor.execute('''
+                            INSERT INTO items (title, user, attributes, price) VALUES (?, ?, ?, ?)
+                        ''', (item['title'], item['user'], json.dumps(item['attributes']), item['price']))
+                        db_conn.commit()
+                        send_discord_notification(item, config.webhook_url)
+                    except sqlite3.IntegrityError:
+                        print(f"Item '{item['title']}' by '{item['user']}' already exists in the database.")
         else:
             print("Fehler beim Abrufen der Seite")
         
         time.sleep(60)
+        driver.quit()
